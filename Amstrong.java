3a78e492217b264827def1bc1ef9fd7919b86429
Manu Krishna <krishnamanu27112004@gmail.com>
2024-10-08 11:19:32 +0530

    update Armstrong.java

--- Amstrong.java
+++ Amstrong.java
@@ -1,31 +1,27 @@
-import java.util.Scanner;
-class Armstrong {
-    public static void main(String[] args) {
-        int num, start, end, i, rem, temp, counter = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the start number: ");
-        start = scanner.nextInt();
-        System.out.print("Enter the end number: ");
-        end = scanner.nextInt();
-        scanner.close(); 
-        for (i = start + 1; i < end; i++) {
-            temp = i;
-            num = 0;
-            while (temp != 0) {
-                rem = temp % 10;
-                num = num + rem * rem * rem;
-                temp = temp / 10;
-            }
-            if (i == num) {
-                if (counter == 0) {
-                    System.out.print("Armstrong Numbers Between " + start + " and " + end + ": ");
-                }
-                System.out.print(i + " ");
-                counter++;
-            }
-        }
-        if (counter == 0) {
-            System.out.println("There is no Armstrong number between " + start + " and " + end);
-        }
-    }
+import java.io.*; 
+import java.lang.Math; 
+import java.util.Scanner; 
+public class Armstrong 
+{ 
+public static void main(String args[]) 
+{ 
+Scanner s= new Scanner(System.in); 
+System.out.print("Enter the lower & upper limit : "); 
+int low = s.nextInt(); 
+int high = s.nextInt(); 
+System.out.print("\nArmstrong numbers within "+low+" and "+high+" is "); 
+for (int i = low+1; i < high; ++i) 
+{ 
+int num=0;
+int dig = (int)Math.log10(i) + 1; 
+for (int temp = i; temp != 0; temp /= 10) 
+{ 
+int rem = temp %10; 
+num += Math.pow(rem, dig); 
+} 
+if (i == num) 
+System.out.print(i + " "); 
+} 
+System.out.println(); 
+} 
 }
