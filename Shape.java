3ea3d30b1d595e3857bf22037e25d92996e72d45
Manu Krishna <krishnamanu27112004@gmail.com>
2025-02-22 10:02:54 +0530

    Update Main.java

--- Shape.java
+++ Shape.java
@@ -1,42 +1,46 @@
 import java.util.Scanner;
+
 abstract class Shape {
-protected double dimension1;
-protected double dimension2;
-public Shape(double dimension1, double dimension2) {
-this.dimension1 = dimension1;
-this.dimension2 = dimension2;
+  protected double dimension1;
+  protected double dimension2;
+  public Shape(double dimension1, double dimension2) {
+    this.dimension1 = dimension1;
+    this.dimension2 = dimension2;
+  }
+  public abstract double area();
 }
-public abstract double area();
-}
+
 class Rectangle extends Shape {
-public Rectangle(double length, double width) {
-super(length, width);
+  public Rectangle(double length, double width) {
+    super(length, width);
+  }
+  public double area() {
+    return dimension1 * dimension2;
+  }
 }
-public double area() {
-return dimension1 * dimension2;
-}
-}
+
 class Triangle extends Shape {
-public Triangle(double base, double height) {
-super(base, height);
+  public Triangle(double base, double height) {
+    super(base, height);
+  }
+  public double area() {
+    return 0.5 * dimension1 * dimension2;
+  }
 }
-public double area() {
-return 0.5 * dimension1 * dimension2;
-}
-}
+
 public class Main {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.println("Enter dimensions for the Rectangle (length and width):");
-double rectLength = scanner.nextDouble();
-double rectWidth = scanner.nextDouble();
-Rectangle rectangle = new Rectangle(rectLength, rectWidth);
-System.out.println("Enter dimensions for the Triangle (base and height):");
-double triBase = scanner.nextDouble();
-double triHeight = scanner.nextDouble();
-Triangle triangle = new Triangle(triBase, triHeight);
-Scanner.close();
-System.out.println("Area of Rectangle: " + rectangle.area());
-System.out.println("Area of Triangle: " + triangle.area());
-}
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter dimensions for the Rectangle (length and width):");
+    double rectLength = scanner.nextDouble();
+    double rectWidth = scanner.nextDouble();
+    Rectangle rectangle = new Rectangle(rectLength, rectWidth);
+    System.out.println("Enter dimensions for the Triangle (base and height):");
+    double triBase = scanner.nextDouble();
+    double triHeight = scanner.nextDouble();
+    Triangle triangle = new Triangle(triBase, triHeight);
+    scanner.close();
+    System.out.println("Area of Rectangle: " + rectangle.area());
+    System.out.println("Area of Triangle: " + triangle.area());
+  }
 }
