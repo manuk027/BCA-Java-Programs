6a96abfda53cbe66a0e1a93a4b7e4b499f45e1f0
Manu Krishna <krishnamanu27112004@gmail.com>
2025-02-23 10:25:58 +0530

    Create ArithmeticExceptionHandling.java

---
+++ 16/ArithmeticExceptionHandling.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ArithmeticExceptionHandling
+{
+  public static void main(String[] args)
+  {
+    Scanner scanner=new Scanner(System.in);
+    System.out.print("Enter the dividend:");
+    int dividend=scanner.nextInt();
+    System.out.print("Enter the divisor:");
+    int divisor=scanner.nextInt();
+    try
+    {
+      int result=divide(dividend, divisor);
+      System.out.println("Result:" + result);
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println("An Arithmetic Exception occured:" + e.getMessage());
+    }
+    finally
+    {
+      scanner.close();
+    }
+  }
+  public static int divide(int dividend, int divisor)
+  {
+    if(divisor==0)
+    {
+      throw new ArithmeticException("Division by zero is not allowed.");
+    }
+    return dividend/divisor;
+  }
+}
