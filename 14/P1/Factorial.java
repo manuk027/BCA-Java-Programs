358a389e56af8218ac8915b420ba4bfc5e657b82
Manu Krishna <krishnamanu27112004@gmail.com>
2025-02-23 10:28:14 +0530

    Create Factorial.java

---
+++ 14/P1/Factorial.java
@@ -0,0 +1,15 @@
+package P1;  
+public class Factorial 
+{  
+  public static long calculateFactorial(int n) 
+  {  
+    if (n == 0 || n == 1) 
+    {  
+      return 1;  
+    } 
+    else 
+    {  
+      return n * calculateFactorial(n - 1);  
+    }
+  }
+}
